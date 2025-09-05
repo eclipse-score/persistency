eclipse-score/inc_mw_per

impl: consistent instance params
- Add `Ignored` mode.
- Consistently use enums.
- Remove bool conversions.

src/rust/rust_kvs/examples/defaults.rs
@@ -1,5 +1,5 @@
 //! Example for default values.
-//! - Creating KVS instance using `KvsBuilder` with `need_defaults` modes.
+//! - Creating KVS instance using `KvsBuilder` with `defaults` modes.
 //! - Default-specific APIs: `get_default_value`, `is_value_default`.
 //! - Key-value operations behavior on defaults available.
 
@@ -42,10 +42,10 @@ fn main() -> Result<(), ErrorCode> {
     create_defaults_file(dir.path().to_path_buf(), instance_id)?;
 
     // Build KVS instance for given instance ID and temporary directory.
-    // `need_defaults` is set to `true` - defaults are required.
+    // `defaults` is set to `Defaults::Required` - defaults are required.
     let builder = KvsBuilder::<Kvs>::new(instance_id)
         .dir(dir_string)
-        .need_defaults(true);
+        .defaults(Defaults::Required);
     let kvs = builder.build()?;
 
     // Set explicit value to `k1`.

src/rust/rust_kvs/src/kvs_mock.rs
@@ -10,9 +10,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::error_code::ErrorCode;
-use crate::kvs_api::FlushOnExit;
-use crate::kvs_api::KvsApi;
-use crate::kvs_api::SnapshotId;
+use crate::kvs_api::{Defaults, FlushOnExit, InstanceId, KvsApi, KvsLoad, SnapshotId};
 use crate::kvs_value::{KvsMap, KvsValue};
 use std::sync::{Arc, Mutex};
 
@@ -33,9 +31,9 @@ impl Default for MockKvs {
 
 impl KvsApi for MockKvs {
     fn open(
-        _instance_id: crate::kvs_api::InstanceId,
-        _need_defaults: crate::kvs_api::OpenNeedDefaults,
-        _need_kvs: crate::kvs_api::OpenNeedKvs,
+        _instance_id: InstanceId,
+        _defaults: Defaults,
+        _kvs_load: KvsLoad,
         _dir: Option<String>,
     ) -> Result<Self, ErrorCode> {
         Ok(MockKvs {
@@ -168,9 +166,9 @@ impl KvsApi for MockKvs {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::kvs_api::KvsApi;
-    use crate::kvs_api::SnapshotId;
     use crate::kvs_value::KvsValue;
+    use KvsApi;
+    use SnapshotId;
 
     #[test]
     fn test_mock_kvs_pass_and_fail_cases() {

src/rust/rust_kvs/tests/cit_multiple_kvs.rs
@@ -24,15 +24,15 @@ fn cit_persistency_multiple_instances() -> Result<(), ErrorCode> {
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             InstanceId(1),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -46,14 +46,14 @@ fn cit_persistency_multiple_instances() -> Result<(), ErrorCode> {
         // Second KVS run.
         let kvs1 = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             InstanceId(1),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -99,15 +99,15 @@ fn cit_persistency_multiple_instances_same_id_common_value() -> Result<(), Error
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -121,14 +121,14 @@ fn cit_persistency_multiple_instances_same_id_common_value() -> Result<(), Error
         // Second KVS run.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -166,15 +166,15 @@ fn cit_persistency_multiple_instances_same_id_interfere() -> Result<(), ErrorCod
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -188,14 +188,14 @@ fn cit_persistency_multiple_instances_same_id_interfere() -> Result<(), ErrorCod
         // Second KVS run.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 

src/rust/rust_kvs/tests/cit_persistency.rs
@@ -42,8 +42,8 @@ fn cit_persistency_flush_on_exit_enabled() -> Result<(), ErrorCode> {
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -59,8 +59,8 @@ fn cit_persistency_flush_on_exit_enabled() -> Result<(), ErrorCode> {
         // KVS file is expected to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            Defaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
 
@@ -102,8 +102,8 @@ fn cit_persistency_flush_on_exit_disabled_drop_data() -> Result<(), ErrorCode> {
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         kvs.set_flush_on_exit(FlushOnExit::No);
@@ -120,8 +120,8 @@ fn cit_persistency_flush_on_exit_disabled_drop_data() -> Result<(), ErrorCode> {
         // KVS file is expected to not to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string),
         )?;
 
@@ -160,8 +160,8 @@ fn cit_persistency_flush_on_exit_disabled_manual_flush() -> Result<(), ErrorCode
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            Defaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         kvs.set_flush_on_exit(FlushOnExit::No);
@@ -181,8 +181,8 @@ fn cit_persistency_flush_on_exit_disabled_manual_flush() -> Result<(), ErrorCode
         // KVS file is expected to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            Defaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
 

src/rust/rust_kvs/tests/cit_snapshots.rs
@@ -18,8 +18,8 @@ fn init_kvs(
 ) -> Result<Kvs, ErrorCode> {
     let kvs = Kvs::open(
         instance_id,
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Optional,
+        Defaults::Optional,
+        KvsLoad::Optional,
         Some(dir_string),
     )?;
 
@@ -42,8 +42,8 @@ fn cit_snapshots_snapshot_count_first_flush() -> Result<(), ErrorCode> {
 
     let kvs = Kvs::open(
         InstanceId(0),
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Optional,
+        Defaults::Optional,
+        KvsLoad::Optional,
         Some(dir_string),
     )?;
     kvs.set_value("counter", 1.0)?;
@@ -69,11 +69,11 @@ fn cit_snapshots_snapshot_count_full() -> Result<(), ErrorCode> {
     for counter in 0..=Kvs::snapshot_max_count() {
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
+            Defaults::Optional,
             if counter == 0 {
-                OpenNeedKvs::Optional
+                KvsLoad::Optional
             } else {
-                OpenNeedKvs::Required
+                KvsLoad::Required
             },
             Some(dir_string.clone()),
         )?;
@@ -86,8 +86,8 @@ fn cit_snapshots_snapshot_count_full() -> Result<(), ErrorCode> {
     {
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            Defaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
         assert_eq!(kvs.snapshot_count(), Kvs::snapshot_max_count());

src/rust/rust_kvs_tool/src/kvs_tool.rs
@@ -528,8 +528,8 @@ fn main() -> Result<(), ErrorCode> {
     let mut args = Arguments::from_env();
 
     let builder = KvsBuilder::new(InstanceId(0))
-        .need_defaults(false)
-        .need_kvs(false);
+        .defaults(Defaults::Optional)
+        .kvs_load(KvsLoad::Optional);
 
     let kvs = match builder.build() {
         Ok(kvs) => kvs,

tests/rust_test_scenarios/src/test_basic.rs
@@ -7,8 +7,8 @@ use tracing::info;
 #[derive(Serialize, Deserialize, Debug)]
 struct KvsParameters {
     instance_id: usize,
-    need_defaults: Option<bool>,
-    need_kvs: Option<bool>,
+    defaults: Option<bool>,
+    kvs_load: Option<bool>,
     dir: Option<String>,
     flush_on_exit: Option<bool>,
 }
@@ -35,11 +35,21 @@ impl Scenario for BasicScenario {
         // Set builder parameters.
         let instance_id = InstanceId(params.instance_id);
         let mut builder = KvsBuilder::new(instance_id);
-        if let Some(flag) = params.need_defaults {
-            builder = builder.need_defaults(flag);
+        if let Some(flag) = params.defaults {
+            let mode = if flag {
+                Defaults::Required
+            } else {
+                Defaults::Optional
+            };
+            builder = builder.defaults(mode);
         }
-        if let Some(flag) = params.need_kvs {
-            builder = builder.need_kvs(flag);
+        if let Some(flag) = params.kvs_load {
+            let mode = if flag {
+                KvsLoad::Required
+            } else {
+                KvsLoad::Optional
+            };
+            builder = builder.kvs_load(mode);
         }
         if let Some(dir) = params.dir {
             builder = builder.dir(dir);

eclipse-score/persistency

src/rust/rust_kvs/examples/defaults.rs
@@ -1,5 +1,5 @@
 //! Example for default values.
-//! - Creating KVS instance using `KvsBuilder` with `need_defaults` modes.
+//! - Creating KVS instance using `KvsBuilder` with `defaults` modes.
 //! - Default-specific APIs: `get_default_value`, `is_value_default`.
 //! - Key-value operations behavior on defaults available.
 
@@ -42,10 +42,10 @@ fn main() -> Result<(), ErrorCode> {
     create_defaults_file(dir.path().to_path_buf(), instance_id)?;
 
     // Build KVS instance for given instance ID and temporary directory.
-    // `need_defaults` is set to `true` - defaults are required.
+    // `defaults` is set to `KvsDefaults::Required` - defaults are required.
     let builder = KvsBuilder::<Kvs>::new(instance_id)
         .dir(dir_string)
-        .need_defaults(true);
+        .defaults(KvsDefaults::Required);
     let kvs = builder.build()?;
 
     // Set explicit value to `k1`.

src/rust/rust_kvs/src/kvs_mock.rs
@@ -10,9 +10,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::error_code::ErrorCode;
-use crate::kvs_api::FlushOnExit;
-use crate::kvs_api::KvsApi;
-use crate::kvs_api::SnapshotId;
+use crate::kvs_api::{FlushOnExit, InstanceId, KvsApi, KvsDefaults, KvsLoad, SnapshotId};
 use crate::kvs_value::{KvsMap, KvsValue};
 use std::sync::{Arc, Mutex};
 
@@ -33,9 +31,9 @@ impl Default for MockKvs {
 
 impl KvsApi for MockKvs {
     fn open(
-        _instance_id: crate::kvs_api::InstanceId,
-        _need_defaults: crate::kvs_api::OpenNeedDefaults,
-        _need_kvs: crate::kvs_api::OpenNeedKvs,
+        _instance_id: InstanceId,
+        _defaults: KvsDefaults,
+        _kvs_load: KvsLoad,
         _dir: Option<String>,
     ) -> Result<Self, ErrorCode> {
         Ok(MockKvs {
@@ -168,9 +166,9 @@ impl KvsApi for MockKvs {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::kvs_api::KvsApi;
-    use crate::kvs_api::SnapshotId;
     use crate::kvs_value::KvsValue;
+    use KvsApi;
+    use SnapshotId;
 
     #[test]
     fn test_mock_kvs_pass_and_fail_cases() {

src/rust/rust_kvs/tests/cit_multiple_kvs.rs
@@ -32,15 +32,15 @@ fn cit_persistency_multiple_instances() -> Result<(), ErrorCode> {
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             InstanceId(1),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -54,14 +54,14 @@ fn cit_persistency_multiple_instances() -> Result<(), ErrorCode> {
         // Second KVS run.
         let kvs1 = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             InstanceId(1),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -112,15 +112,15 @@ fn cit_persistency_multiple_instances_same_id_common_value() -> Result<(), Error
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -134,14 +134,14 @@ fn cit_persistency_multiple_instances_same_id_common_value() -> Result<(), Error
         // Second KVS run.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -184,15 +184,15 @@ fn cit_persistency_multiple_instances_same_id_interfere() -> Result<(), ErrorCod
         // Create first KVS instance.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         // Create second KVS instance.
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -206,14 +206,14 @@ fn cit_persistency_multiple_instances_same_id_interfere() -> Result<(), ErrorCod
         // Second KVS run.
         let kvs1 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         let kvs2 = Kvs::open(
             instance_id,
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 

src/rust/rust_kvs/tests/cit_persistency.rs
@@ -50,8 +50,8 @@ fn cit_persistency_flush_on_exit_enabled() -> Result<(), ErrorCode> {
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
 
@@ -67,8 +67,8 @@ fn cit_persistency_flush_on_exit_enabled() -> Result<(), ErrorCode> {
         // KVS file is expected to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            KvsDefaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
 
@@ -115,8 +115,8 @@ fn cit_persistency_flush_on_exit_disabled_drop_data() -> Result<(), ErrorCode> {
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         kvs.set_flush_on_exit(FlushOnExit::No);
@@ -133,8 +133,8 @@ fn cit_persistency_flush_on_exit_disabled_drop_data() -> Result<(), ErrorCode> {
         // KVS file is expected to not to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string),
         )?;
 
@@ -178,8 +178,8 @@ fn cit_persistency_flush_on_exit_disabled_manual_flush() -> Result<(), ErrorCode
         // First KVS run.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Optional,
+            KvsDefaults::Optional,
+            KvsLoad::Optional,
             Some(dir_string.clone()),
         )?;
         kvs.set_flush_on_exit(FlushOnExit::No);
@@ -199,8 +199,8 @@ fn cit_persistency_flush_on_exit_disabled_manual_flush() -> Result<(), ErrorCode
         // KVS file is expected to exist.
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            KvsDefaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
 

src/rust/rust_kvs/tests/cit_snapshots.rs
@@ -22,8 +22,8 @@ fn init_kvs(
 ) -> Result<Kvs, ErrorCode> {
     let kvs = Kvs::open(
         instance_id,
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Optional,
+        KvsDefaults::Optional,
+        KvsLoad::Optional,
         Some(dir_string),
     )?;
 
@@ -50,8 +50,8 @@ fn cit_snapshots_snapshot_count_first_flush() -> Result<(), ErrorCode> {
 
     let kvs = Kvs::open(
         InstanceId(0),
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Optional,
+        KvsDefaults::Optional,
+        KvsLoad::Optional,
         Some(dir_string),
     )?;
     kvs.set_value("counter", 1.0)?;
@@ -82,11 +82,11 @@ fn cit_snapshots_snapshot_count_full() -> Result<(), ErrorCode> {
     for counter in 0..=Kvs::snapshot_max_count() {
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
+            KvsDefaults::Optional,
             if counter == 0 {
-                OpenNeedKvs::Optional
+                KvsLoad::Optional
             } else {
-                OpenNeedKvs::Required
+                KvsLoad::Required
             },
             Some(dir_string.clone()),
         )?;
@@ -99,8 +99,8 @@ fn cit_snapshots_snapshot_count_full() -> Result<(), ErrorCode> {
     {
         let kvs = Kvs::open(
             InstanceId(0),
-            OpenNeedDefaults::Optional,
-            OpenNeedKvs::Required,
+            KvsDefaults::Optional,
+            KvsLoad::Required,
             Some(dir_string),
         )?;
         assert_eq!(kvs.snapshot_count(), Kvs::snapshot_max_count());

src/rust/rust_kvs_tool/src/kvs_tool.rs
@@ -528,8 +528,8 @@ fn main() -> Result<(), ErrorCode> {
     let mut args = Arguments::from_env();
 
     let builder = KvsBuilder::new(InstanceId(0))
-        .need_defaults(false)
-        .need_kvs(false);
+        .defaults(KvsDefaults::Optional)
+        .kvs_load(KvsLoad::Optional);
 
     let kvs = match builder.build() {
         Ok(kvs) => kvs,

tests/rust_test_scenarios/src/test_basic.rs
@@ -23,17 +23,11 @@ impl Scenario for BasicScenario {
 
         // Set builder parameters.
         let mut builder = KvsBuilder::new(params.instance_id);
-        if let Some(flag) = params.need_defaults {
-            builder = builder.need_defaults(match flag {
-                OpenNeedDefaults::Optional => false,
-                OpenNeedDefaults::Required => true,
-            });
+        if let Some(flag) = params.defaults {
+            builder = builder.defaults(flag);
         }
-        if let Some(flag) = params.need_kvs {
-            builder = builder.need_kvs(match flag {
-                OpenNeedKvs::Optional => false,
-                OpenNeedKvs::Required => true,
-            });
+        if let Some(flag) = params.kvs_load {
+            builder = builder.kvs_load(flag);
         }
         if let Some(dir) = params.dir {
             builder = builder.dir(dir.to_string_lossy().to_string());