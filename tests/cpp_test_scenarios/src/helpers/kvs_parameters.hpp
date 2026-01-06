263500f0eb9e4fc66abd22619cbe4d5ae0f9898d
Saumya-R <saumya.rai@qorix.ai>
2026-01-06 13:26:24 +0530

    adding copyrights

--- tests/cpp_test_scenarios/src/helpers/kvs_parameters.hpp
+++ tests/cpp_test_scenarios/src/helpers/kvs_parameters.hpp
@@ -1,4 +1,3 @@
-
 /********************************************************************************
  * Copyright (c) 2025 Contributors to the Eclipse Foundation
  *
@@ -11,6 +10,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
+
 #pragma once
 
 #include <kvs.hpp>
